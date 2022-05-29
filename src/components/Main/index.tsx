79bb0962c0c79c03329c1aa5f944ee7b4aa781a8
DABIN JEONG <41@b41.kr>
2022-05-29 22:31:56 +0900

    [Add] Index 페이지 마크업

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,23 +1,55 @@
-import React from 'react';
-import { Row, Button, Col } from 'antd';
+import React, { ReactElement, useState } from 'react';
+import { Row, Button, Col, Layout, Space, Typography } from 'antd';
+import LoginBox from './LoginBox';
 
 type Props = {};
 
-const Main = (props: Props) => {
+const Main = (props: Props): ReactElement => {
+  const { Content, Footer } = Layout;
+  const { Title } = Typography;
+  const [isLoginBox, setIsLoginBox] = useState<boolean>(false);
+
+  const handleLoginBox = (): void => {
+    setIsLoginBox(!isLoginBox);
+  };
+
   return (
-    <div>
-      <Col>
-        <Row>듣기 시험</Row>
-        <Row>플레이리스트</Row>
-        <Row>만들기</Row>
-        <Row>
-          <Button>로그인</Button>
+    <Layout>
+      <Content>
+        <Row justify="center" align="middle" style={{ margin: '30px auto' }}>
+          <Col>
+            <Space
+              direction="vertical"
+              size={1}
+              style={{ margin: '10px auto' }}
+            >
+              <Row>
+                <Title>
+                  듣기 시험 <br /> 플레이리스트 <br /> 만들기
+                </Title>
+              </Row>
+            </Space>
+            <Row justify="center">
+              <Space direction="vertical" size={5} align="center">
+                {isLoginBox ? (
+                  <LoginBox />
+                ) : (
+                  <Button size="large" shape="round" onClick={handleLoginBox}>
+                    로그인
+                  </Button>
+                )}
+                <Button size="large" shape="round">
+                  듣플만이란?
+                </Button>
+              </Space>
+            </Row>
+          </Col>
         </Row>
-        <Row>
-          <Button>듣플만이란?</Button>
-        </Row>
-      </Col>
-    </div>
+      </Content>
+      <Footer style={{ backgroundColor: 'white', textAlign: 'center' }}>
+        듣기시험 플레이리스트 만들기
+      </Footer>
+    </Layout>
   );
 };
 
