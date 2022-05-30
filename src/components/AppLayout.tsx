f3dd18926a053efd62a26040b418b9babf742bab
DABIN JEONG <41@b41.kr>
2022-05-30 22:27:33 +0900

    [Add] 로그아웃 버튼 추가

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { signOut } from 'firebase/auth';
+import { auth } from '../fbase';
 
-type Props = {}
+type Props = {};
 
 const AppLayout = (props: Props) => {
+  const Logout = async () => {
+    await signOut(auth)
+      .then(() => {
+        console.log('로그아웃 성공');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   return (
-    <div>AppLayout</div>
-  )
-}
+    <div>
+      <button onClick={Logout}>로그아웃</button>
+      AppLayout
+    </div>
+  );
+};
 
-export default AppLayout
+export default AppLayout;
