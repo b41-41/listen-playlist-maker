55721d13a45eb43af3984c21bb16f6699ef87bae
DABIN JEONG <41@b41.kr>
2022-06-08 22:39:13 +0900

    [Style] 공통 레이아웃 추가

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { ReactComponentElement, ReactElement } from 'react';
 import { signOut } from 'firebase/auth';
 import { auth } from '../fbase';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { Layout, Row, Button, Typography } from 'antd';
+import Manage from './Manage';
 
 type Props = {};
+
+const { Header, Footer, Content } = Layout;
+const { Text } = Typography;
 
 const AppLayout = (props: Props) => {
   const Logout = async () => {
@@ -15,10 +22,23 @@ const AppLayout = (props: Props) => {
       });
   };
   return (
-    <div>
-      <button onClick={Logout}>로그아웃</button>
-      AppLayout
-    </div>
+    <Layout>
+      <Header style={{ color: 'white' }}>
+        <Row justify="space-between" align="middle">
+          <div>듣플만</div>
+          <Button onClick={Logout}>로그아웃</Button>
+        </Row>
+      </Header>
+      <Content style={{ textAlign: 'center' }}>
+        <Routes>
+          <Route path="/" element={<>안녕</>} />
+          <Route path="/manage" element={<Manage />} />
+        </Routes>
+      </Content>
+      <Footer style={{ backgroundColor: 'white' }}>
+        <Text>문의: malgolil41@gmail.com</Text>
+      </Footer>
+    </Layout>
   );
 };
 
